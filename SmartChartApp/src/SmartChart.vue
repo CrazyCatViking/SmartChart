e3d61584f4f0444298e3403af2008e3fff9d0e4f
Kristoffer Jansen <krissjansen@gmail.com>
2023-04-18 23:22:28 +0200

    Complete splitting canvas into separate package

--- SmartChartApp/src/SmartChart.vue
+++ SmartChartApp/src/SmartChart.vue
@@ -4,9 +4,8 @@
 </template>
 
 <script setup lang="ts">
-import Canvas from './SmartChart/Canvas.vue';
 import Toolbar from './Toolbar/Toolbar.vue';
-import { useCanvas } from './SmartChart/useCanvas';
+import { useCanvas, Canvas } from 'smartcanvas';
 
 const { chart } = useCanvas();
 </script>
--- SmartChartApp/src/SmartChart/elements/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './ellipse';
-export * from './image';
-export * from './rectangle';
-export * from './element';
-export * from './text';
-export * from './elementGroup';
-export * from './elementConnector';
